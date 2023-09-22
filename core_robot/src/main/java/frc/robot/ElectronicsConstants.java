0209434bc90299bcf68094de4f57f4a03982f185
irs1318ds <issaquahroboticssociety@outlook.com>
2023-09-21 20:24:37 -0700

    KitBot Stuff

--- core_robot/src/main/java/frc/robot/ElectronicsConstants.java
+++ core_robot/src/main/java/frc/robot/ElectronicsConstants.java
@@ -57,8 +57,8 @@ public class ElectronicsConstants
     public static final int INDICATOR_LIGHT_CANDLE_CAN_ID = 55;
 
     // TankDriveTrain
-    public static final int SIMPLE_TANK_DRIVETRAIN_LEFT_PWM_CHANNEL = 8;
-    public static final int SIMPLE_TANK_DRIVETRAIN_RIGHT_PWM_CHANNEL = 9;
+    public static final int SIMPLE_TANK_DRIVETRAIN_LEFT_PWM_CHANNEL = 7;
+    public static final int SIMPLE_TANK_DRIVETRAIN_RIGHT_PWM_CHANNEL = 6;
 
     //================================================== SDSDriveTrain ==============================================================
 

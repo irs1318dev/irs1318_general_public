379689b8264f3dd1f580bef1639b0c819ea465a1
Will Rall <wm_rall@hotmail.com>
2023-10-12 16:36:22 -0700

    Add a way to control spark max using SmartDashboard

--- core_robot/src/main/java/frc/robot/driver/SmartDashboardSelectionManager.java
+++ core_robot/src/main/java/frc/robot/driver/SmartDashboardSelectionManager.java
@@ -11,6 +11,16 @@ public class SmartDashboardSelectionManager
     private final ISendableChooser<StartPosition> positionChooser;
     private final ISendableChooser<AutoRoutine> routineChooser;
 
+    private final ISendableChooser<MotorMode> motorModeChooser;
+    private final IDoubleSubscriber kpSlider;
+    private final IDoubleSubscriber kiSlider;
+    private final IDoubleSubscriber kdSlider;
+    private final IDoubleSubscriber kfSlider;
+    private final IDoubleSubscriber kAccelSlider;
+    private final IDoubleSubscriber kCruiseVelSlider;
+    private final IBooleanSubscriber invertOutputCheckbox;
+    private final IBooleanSubscriber brakeModeCheckbox;
+
     public enum StartPosition
     {
         Mid,
@@ -30,6 +40,14 @@ public class SmartDashboardSelectionManager
         OnePickupCharge,
         OnePlusOne,
         ThreePiece
+    }
+
+    public enum MotorMode
+    {
+        PercentOutput,
+        PositionPID,
+        VelocityPID,
+        TrapezoidalMotionProfile,
     }
 
     /**
@@ -59,6 +77,23 @@ public class SmartDashboardSelectionManager
         this.positionChooser.addObject("load", StartPosition.Load);
         this.positionChooser.addObject("guard", StartPosition.Guard);
         networkTableProvider.addChooser("Start Position", this.positionChooser);
+
+        this.motorModeChooser = networkTableProvider.getSendableChooser();
+        this.motorModeChooser.addDefault("percentOutput", MotorMode.PercentOutput);
+        this.motorModeChooser.addObject("positionPID", MotorMode.PositionPID);
+        this.motorModeChooser.addObject("velocityPID", MotorMode.VelocityPID);
+        this.motorModeChooser.addObject("trapezoidalMotionProfile", MotorMode.TrapezoidalMotionProfile);
+        networkTableProvider.addChooser("Motor Mode", this.motorModeChooser);
+
+        this.kpSlider = networkTableProvider.getNumberSlider("kP", 0.0);
+        this.kiSlider = networkTableProvider.getNumberSlider("kI", 0.0);
+        this.kdSlider = networkTableProvider.getNumberSlider("kD", 0.0);
+        this.kfSlider = networkTableProvider.getNumberSlider("kF", 0.0);
+        this.kAccelSlider = networkTableProvider.getNumberSlider("kAccel", 0.0);
+        this.kCruiseVelSlider = networkTableProvider.getNumberSlider("kCruiseVel", 0.0);
+
+        this.invertOutputCheckbox = networkTableProvider.getCheckbox("invertOutput", false);
+        this.brakeModeCheckbox = networkTableProvider.getCheckbox("useBrakeMode", false);
     }
 
     public StartPosition getSelectedStartPosition()
@@ -69,6 +104,51 @@ public class SmartDashboardSelectionManager
     public AutoRoutine getSelectedAutoRoutine()
     {
         return SmartDashboardSelectionManager.GetSelectedOrDefault(this.routineChooser, AutoRoutine.None);
+    }
+
+    public MotorMode getSelectedMotorMode()
+    {
+        return SmartDashboardSelectionManager.GetSelectedOrDefault(this.motorModeChooser, MotorMode.PercentOutput);
+    }
+
+    public double getSelectedKP()
+    {
+        return this.kpSlider.get();
+    }
+
+    public double getSelectedKI()
+    {
+        return this.kiSlider.get();
+    }
+
+    public double getSelectedKD()
+    {
+        return this.kdSlider.get();
+    }
+
+    public double getSelectedKF()
+    {
+        return this.kfSlider.get();
+    }
+
+    public double getSelectedAcceleration()
+    {
+        return this.kAccelSlider.get();
+    }
+
+    public double getSelectedCruiseVelocity()
+    {
+        return this.kCruiseVelSlider.get();
+    }
+
+    public boolean getSelectedInvertOutput()
+    {
+        return this.invertOutputCheckbox.get();
+    }
+
+    public boolean getSelectedBrakeMode()
+    {
+        return this.brakeModeCheckbox.get();
     }
 
     private static <T> T GetSelectedOrDefault(ISendableChooser<T> chooser, T defaultValue)
--- core_robot/src/main/java/frc/robot/mechanisms/OneMotorSparkMechanism.java
+++ core_robot/src/main/java/frc/robot/mechanisms/OneMotorSparkMechanism.java
@@ -17,94 +17,74 @@ public class OneMotorSparkMechanism implements IMechanism
     private final IDriver driver;
     private final ILogger logger;
     private final ISparkMax motor;
+    private final SmartDashboardSelectionManager selectionManager;
 
     private double velocity;
     private double position;
-    public boolean reverseLimitSwtichStatus;
-    public boolean forwardLimitSwitchStatus;
+    private boolean reverseLimitSwtichStatus;
+    private boolean forwardLimitSwitchStatus;
+
+    private SmartDashboardSelectionManager.MotorMode currentMode;
+    private double kP;
+    private double kI;
+    private double kD;
+    private double kF;
+    private double kAccel;
+    private double kCruiseVel;
+    private boolean useBrakeMode;
+    private boolean invertOutput;
 
     @Inject
     public OneMotorSparkMechanism(
         IDriver driver,
         LoggingManager logger,
-        IRobotProvider provider)
+        IRobotProvider provider,
+        SmartDashboardSelectionManager selectionManager)
     {
         this.driver = driver;
         this.logger = logger;
+        this.selectionManager = selectionManager;
         this.motor = provider.getSparkMax(ElectronicsConstants.ONEMOTOR_PRIMARY_MOTOR_CHANNEL, SparkMaxMotorType.Brushless);
+        this.motor.setRelativeEncoder();
         this.motor.setNeutralMode(MotorNeutralMode.Brake);
         this.motor.setInvertOutput(TuningConstants.ONEMOTOR_INVERT_OUTPUT);
         this.motor.reset();
 
-        ISparkMax follower = provider.getSparkMax(ElectronicsConstants.ONEMOTOR_FOLLOWER_MOTOR_CHANNEL, SparkMaxMotorType.Brushless);
-        follower.setNeutralMode(MotorNeutralMode.Brake);
-        follower.reset();
-        follower.follow(this.motor);
+        this.motor.setControlMode(SparkMaxControlMode.PercentOutput);
+        this.motor.setPIDFSmartMotion(
+            TuningConstants.ONEMOTOR_PID_KP,
+            TuningConstants.ONEMOTOR_PID_KI,
+            TuningConstants.ONEMOTOR_PID_KD,
+            TuningConstants.ONEMOTOR_PID_KF,
+            0,
+            TuningConstants.ONEMOTOR_PID_MM_CRUISE_VELOC,
+            TuningConstants.ONEMOTOR_PID_MM_ACCEL,
+            TuningConstants.ONEMOTOR_PID_MIN_OUTPUT,
+            TuningConstants.ONEMOTOR_PID_MAX_OUTPUT,
+            OneMotorSparkMechanism.slotId);
 
-        this.motor.setForwardLimitSwitch(
-            TuningConstants.ONEMOTOR_FORWARD_LIMIT_SWITCH_ENABLED,
-            TuningConstants.ONEMOTOR_FORWARD_LIMIT_SWITCH_NORMALLY_OPEN);
-        this.motor.setReverseLimitSwitch(
-            TuningConstants.ONEMOTOR_REVERSE_LIMIT_SWITCH_ENABLED,
-            TuningConstants.ONEMOTOR_REVERSE_LIMIT_SWITCH_NORMALLY_OPEN);
+        this.currentMode = SmartDashboardSelectionManager.MotorMode.PercentOutput;
+        this.kP = TuningConstants.ONEMOTOR_PID_KP;
+        this.kI = TuningConstants.ONEMOTOR_PID_KI;
+        this.kD = TuningConstants.ONEMOTOR_PID_KD;
+        this.kF = TuningConstants.ONEMOTOR_PID_KF;
+        this.kAccel = TuningConstants.ONEMOTOR_PID_MM_ACCEL;
+        this.kCruiseVel = TuningConstants.ONEMOTOR_PID_MM_CRUISE_VELOC;
+        this.useBrakeMode = true;
+        this.invertOutput = TuningConstants.ONEMOTOR_INVERT_OUTPUT;
 
-        if (TuningConstants.ONEMOTOR_USE_PID)
+        if (TuningConstants.ONEMOTOR_FORWARD_LIMIT_SWITCH_ENABLED || TuningConstants.ONEMOTOR_REVERSE_LIMIT_SWITCH_ENABLED)
         {
-            if (TuningConstants.ONEMOTOR_PID_POSITIONAL)
-            {
-                if (TuningConstants.ONEMOTOR_PID_POSITIONAL_MM)
-                {
-                    this.motor.setControlMode(SparkMaxControlMode.Position);
-
-                    this.motor.setPIDFSmartMotion(
-                        TuningConstants.ONEMOTOR_PID_KP,
-                        TuningConstants.ONEMOTOR_PID_KI,
-                        TuningConstants.ONEMOTOR_PID_KD,
-                        TuningConstants.ONEMOTOR_PID_KF,
-                        0,
-                        TuningConstants.ONEMOTOR_PID_MM_CRUISE_VELOC,
-                        TuningConstants.ONEMOTOR_PID_MM_ACCEL,
-                        TuningConstants.ONEMOTOR_PID_MIN_OUTPUT,
-                        TuningConstants.ONEMOTOR_PID_MAX_OUTPUT,
-                        OneMotorSparkMechanism.slotId);
-                }
-                else
-                {
-                    this.motor.setControlMode(SparkMaxControlMode.Position);
-
-                    this.motor.setPIDF(
-                        TuningConstants.ONEMOTOR_PID_KP,
-                        TuningConstants.ONEMOTOR_PID_KI,
-                        TuningConstants.ONEMOTOR_PID_KD,
-                        TuningConstants.ONEMOTOR_PID_KF,
-                        TuningConstants.ONEMOTOR_PID_MIN_OUTPUT,
-                        TuningConstants.ONEMOTOR_PID_MAX_OUTPUT,
-                        OneMotorSparkMechanism.slotId);
-                }
-            }
-            else
-            {
-                this.motor.setControlMode(SparkMaxControlMode.Velocity);
-
-                this.motor.setPIDF(
-                    TuningConstants.ONEMOTOR_PID_KP,
-                    TuningConstants.ONEMOTOR_PID_KI,
-                    TuningConstants.ONEMOTOR_PID_KD,
-                    TuningConstants.ONEMOTOR_PID_KF,
-                    TuningConstants.ONEMOTOR_PID_MIN_OUTPUT,
-                    TuningConstants.ONEMOTOR_PID_MAX_OUTPUT,
-                    OneMotorSparkMechanism.slotId);
-            }
-        }
-        else
-        {
-            this.motor.setControlMode(SparkMaxControlMode.PercentOutput);
+            this.motor.setForwardLimitSwitch(
+                TuningConstants.ONEMOTOR_FORWARD_LIMIT_SWITCH_ENABLED,
+                TuningConstants.ONEMOTOR_FORWARD_LIMIT_SWITCH_NORMALLY_OPEN);
+            this.motor.setReverseLimitSwitch(
+                TuningConstants.ONEMOTOR_REVERSE_LIMIT_SWITCH_ENABLED,
+                TuningConstants.ONEMOTOR_REVERSE_LIMIT_SWITCH_NORMALLY_OPEN);
         }
 
         this.velocity = 0.0;
         this.position = 0.0;
-        this.reverseLimitSwtichStatus = false;
-        this.forwardLimitSwitchStatus = false;
     }
 
     @Override
@@ -128,19 +108,116 @@ public class OneMotorSparkMechanism implements IMechanism
     @Override
     public void update()
     {
+        boolean shouldUpdatePID = false;
+        double newKP = this.selectionManager.getSelectedKP();
+        if (newKP != this.kP)
+        {
+            shouldUpdatePID = true;
+        }
+
+        double newKI = this.selectionManager.getSelectedKI();
+        if (newKI != this.kI)
+        {
+            shouldUpdatePID = true;
+        }
+
+        double newKD = this.selectionManager.getSelectedKD();
+        if (newKD != this.kD)
+        {
+            shouldUpdatePID = true;
+        }
+
+        double newKF = this.selectionManager.getSelectedKF();
+        if (newKF != this.kF)
+        {
+            shouldUpdatePID = true;
+        }
+
+        double newCruiseVel = this.selectionManager.getSelectedCruiseVelocity();
+        if (newCruiseVel != this.kCruiseVel)
+        {
+            shouldUpdatePID = true;
+        }
+
+        double newAccel = this.selectionManager.getSelectedAcceleration();
+        if (newAccel != this.kAccel)
+        {
+            shouldUpdatePID = true;
+        }
+
+        if (shouldUpdatePID)
+        {
+            this.kP = newKP;
+            this.kI = newKI;
+            this.kD = newKD;
+            this.kF = newKF;
+            this.kAccel = newAccel;
+            this.kCruiseVel = newCruiseVel;
+
+            this.motor.setPIDFSmartMotion(
+                this.kP,
+                this.kI,
+                this.kD,
+                this.kF,
+                0,
+                (int)this.kCruiseVel,
+                (int)this.kAccel,
+                TuningConstants.ONEMOTOR_PID_MIN_OUTPUT,
+                TuningConstants.ONEMOTOR_PID_MAX_OUTPUT,
+                OneMotorSparkMechanism.slotId);
+        }
+
+        boolean newInvertOutput = this.selectionManager.getSelectedInvertOutput();
+        if (newInvertOutput != this.invertOutput)
+        {
+            this.invertOutput = newInvertOutput;
+            this.motor.setInvertOutput(TuningConstants.ONEMOTOR_INVERT_OUTPUT);
+        }
+
+        boolean newBrakeMode = this.selectionManager.getSelectedBrakeMode();
+        if (newBrakeMode != this.useBrakeMode)
+        {
+            this.useBrakeMode = newBrakeMode;
+            this.motor.setNeutralMode(this.useBrakeMode ? MotorNeutralMode.Brake : MotorNeutralMode.Coast);
+        }
+
+        SmartDashboardSelectionManager.MotorMode newMode = this.selectionManager.getSelectedMotorMode();
+        if (newMode != this.currentMode)
+        {
+            this.currentMode = newMode;
+            switch (this.currentMode)
+            {
+                case PercentOutput:
+                    this.motor.setControlMode(SparkMaxControlMode.PercentOutput);
+                    break;
+
+                case PositionPID:
+                    this.motor.setControlMode(SparkMaxControlMode.Position);
+                    break;
+
+                case VelocityPID:
+                    this.motor.setControlMode(SparkMaxControlMode.Velocity);
+                    break;
+
+                case TrapezoidalMotionProfile:
+                    this.motor.setControlMode(SparkMaxControlMode.SmartMotionPosition);
+                    break;
+            }
+        }
+
         double setpoint = this.driver.getAnalog(AnalogOperation.OneMotorPower);
 
         double maxSetpointValue = 1.0;
-        if (TuningConstants.ONEMOTOR_USE_PID)
+        switch (this.currentMode)
         {
-            if (TuningConstants.ONEMOTOR_PID_POSITIONAL)
-            {
+            case PositionPID:
+            case TrapezoidalMotionProfile:
                 maxSetpointValue = TuningConstants.ONEMOTOR_PID_MAX_POSITION;
-            }
-            else
-            {
+                break;
+
+            case VelocityPID:
                 maxSetpointValue = TuningConstants.ONEMOTOR_PID_MAX_VELOCITY;
-            }
+                break;
         }
 
         setpoint *= maxSetpointValue;

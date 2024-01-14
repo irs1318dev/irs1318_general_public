9448709c01e42a1b2225e919b07085577095ba77
William Rall <wm_rall@hotmail.com>
2024-01-13 17:16:54 -0800

    fix minor issue with TalonFXWrapper

--- robot_roborio/src/main/java/frc/lib/robotprovider/TalonFXWrapper.java
+++ robot_roborio/src/main/java/frc/lib/robotprovider/TalonFXWrapper.java
@@ -749,7 +749,7 @@ public class TalonFXWrapper implements ITalonFX
         }
 
         this.error.refresh();
-        CTREStatusCodeHelper.printError(this.position.getStatus(), "TalonFX.getError");
+        CTREStatusCodeHelper.printError(this.error.getStatus(), "TalonFX.getError");
 
         return this.error.getValue();
     }
